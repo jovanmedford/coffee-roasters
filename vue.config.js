93e9a3752004c2a4d50537122b15df998d9bbb65
Jovan Medford <jovanmedford@gmail.com>
2021-05-14 11:28:10 -0400

    Fix: sass global imports
    typography was spelt incorrectly and functions import was missing

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,8 @@ module.exports = {
       sass: {
         prependData: `
               @import "@/styles/_variables.scss";
-              @import "@/styles/_typgraphy.scss";`,
+              @import "@/styles/_typography.scss"
+              @import "@/styles/_functions.scss";`,
       },
     },
   },
