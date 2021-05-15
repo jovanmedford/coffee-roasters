4fe5047b78d3da35b62ebaa7e53ed24f4c3c4c09
Jovan Medford <jovanmedford@gmail.com>
2021-05-15 11:46:34 -0400

    Update body styles
    Change background colour and remove margin

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@
 </template>
 
 <style lang="scss">
-#app {
+body {
   font-family: Barlow, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: $color-text;
+  background-color: $color-background;
+  margin: 0;
 }
 </style>
