af9cdffc733110e1131917f559f2056ecf9f9a79
Jovan Medford <jovanmedford@gmail.com>
2021-05-18 15:07:41 -0400

    Feat: Add Benefits Section to home page

---
+++ src/components/Benefit.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="benefit">
+    <img class="benefit__icon" :src="benefitIcon" />
+    <div class="benefit__content">
+      <h3 class="benefit__title">{{ title }}</h3>
+      <p class="benefit__description">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Benefit",
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: "Best Quality",
+    },
+    description: {
+      type: String,
+      required: true,
+      default:
+        "Discover an endless variety of the world’s best artisan coffee from each of our roasters.",
+    },
+    iconSrc: {
+      type: String,
+      required: true,
+      default: "home/desktop/icon-coffee-bean.svg",
+    },
+  },
+  computed: {
+    benefitIcon() {
+      return require(`../assets/${this.iconSrc}`);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.benefit {
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  background-color: $color-primary;
+  padding: rem-calc(72 12 48 12);
+  border-radius: 8px;
+  margin-bottom: rem-calc(24);
+
+  @media screen and (min-width: $breakpoint-tablet) {
+    flex-direction: row;
+    justify-content: space-evenly;
+    padding: rem-calc(36 48 48 72);
+    text-align: left;
+  }
+
+  @media screen and (min-width: $breakpoint-desktop) {
+    flex-direction: column;
+    justify-content: space-around;
+    padding: rem-calc(72 40);
+    text-align: center;
+    margin-bottom: 0;
+
+    &:not(:last-child) {
+      margin-right: rem-calc(32);
+    }
+  }
+}
+
+.benefit__icon {
+  width: rem-calc(56);
+  height: rem-calc(56);
+  margin-bottom: rem-calc(56);
+
+  @media screen and (min-width: $breakpoint-tablet) {
+    margin-bottom: 0;
+  }
+
+  @media screen and (min-width: $breakpoint-desktop) {
+    margin-bottom: rem-calc(56);
+  }
+}
+
+.benefit__content {
+  max-width: rem-calc(256);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  @media screen and (min-width: $breakpoint-tablet) {
+    max-width: rem-calc(344);
+  }
+}
+
+.benefit__title {
+  font-size: rem-calc(24);
+  margin-bottom: rem-calc(16);
+}
+
+.benefit__description {
+  color: $color-background;
+}
+</style>
---
+++ src/components/SectionBenefits.vue
@@ -0,0 +1,73 @@
+<template>
+  <section clsss="benefits-section">
+    <div class="benefits-section__content">
+      <h2 class="benefits-section__title">Why choose us?</h2>
+      <p class="benefits-section__description">
+        A large part of our role is choosing which particular coffees will be
+        featured in our range. This means working closely with the best coffee
+        growers to give you a more impactful experience on every level.
+      </p>
+    </div>
+    <BenefitList />
+  </section>
+</template>
+
+<script>
+import BenefitList from "@/components/BenefitList.vue";
+
+export default {
+  name: "SectionBenefits",
+  components: {
+    BenefitList,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+section {
+  display: block;
+  background-color: #2c343e;
+  color: white;
+  padding: rem-calc(64 24 664 24);
+  margin: rem-calc(0 12 717 12);
+  position: relative;
+
+  @media screen and (min-width: $breakpoint-tablet) {
+    padding: rem-calc(56 72 296 72);
+    margin: rem-calc(0 40 460 40);
+  }
+
+  @media screen and (min-width: $breakpoint-desktop) {
+    padding: rem-calc(104 320 317 320);
+    margin: rem-calc(0 40 352 40);
+  }
+}
+
+.benefits-section__content {
+  text-align: center;
+  margin-bottom: rem-calc(64);
+
+  @media screen and (min-width: $breakpoint-tablet) {
+    margin-bottom: rem-calc(72);
+  }
+
+  @media screen and (min-width: $breakpoint-desktop) {
+    margin: 0 auto rem-calc(86) auto;
+    max-width: rem-calc(540);
+  }
+}
+
+.benefits-section__title {
+  font-size: rem-calc(32);
+  margin-bottom: rem-calc(24);
+}
+
+.benefits-section__description {
+  font-size: rem-calc(16);
+  line-height: rem-calc(25);
+}
+
+.benefits-list {
+  position: absolute;
+}
+</style>
