1e1502f19723d7072e57dd208ace1442904e273d
Jovan Medford <jovanmedford@gmail.com>
2021-05-19 11:09:20 -0400

    Refactor: Make hero button into component BaseButton

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button v-if="text" class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  props: {
+    text: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  padding: rem-calc(16 32);
+  background-color: $color-primary;
+  border: none;
+  color: white;
+  font-weight: 700;
+  font-size: rem-calc(18);
+  font-family: Fraunces;
+  border-radius: 6px;
+
+  &:hover {
+    background-color: $color-primary-light;
+    cursor: pointer;
+  }
+}
+</style>
