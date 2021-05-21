99cc5d14c89c1d6a412918f4cf15d172fe5d7a86
Jovan Medford <jovanmedford@gmail.com>
2021-05-21 12:40:54 -0400

    Feat: Add Location section to about us page

---
+++ src/components/Location.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="location">
+    <img class="location__image" :src="locationImage" />
+    <h3 class="location__country-name">{{ country }}</h3>
+    <ul class="location__address">
+      <li class="location__address-line">{{ streetNumber }}</li>
+      <li class="location__address-line">{{ city }}</li>
+      <li class="location__address-line">{{ postalCode }}</li>
+      <li class="location__address-line">{{ contactNumber }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Location",
+  props: {
+    imageSrc: {
+      type: String,
+      required: true,
+      default: "about/desktop/illustration-australia.svg",
+    },
+    country: {
+      type: String,
+      required: true,
+      default: "Australia",
+    },
+    streetNumber: {
+      type: String,
+      required: true,
+      default: "68 Asfordby Rd",
+    },
+    city: {
+      type: String,
+      required: true,
+      default: "Alcaston",
+    },
+    postalCode: {
+      type: String,
+      required: true,
+      default: "SY6 1YA",
+    },
+    contactNumber: {
+      type: String,
+      required: true,
+      default: "+44 1241 918425",
+    },
+  },
+  computed: {
+    locationImage() {
+      return require(`../assets/${this.imageSrc}`);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.location {
+  margin-bottom: rem-calc(80);
+
+  @media screen and (min-width: $breakpoint-tablet) {
+    margin-right: rem-calc(32);
+    margin-bottom: 0;
+  }
+
+  @media screen and (min-width: $breakpoint-tablet) {
+    margin-right: rem-calc(95);
+    margin-bottom: 0;
+  }
+}
+
+.location__image {
+  margin-bottom: rem-calc(48);
+  width: rem-calc(48);
+  height: rem-calc(44);
+}
+
+.location__address {
+  list-style: none;
+
+  line-height: rem-calc(25);
+
+  @media screen and (min-width: $breakpoint-tablet) {
+    line-height: rem-calc(26);
+  }
+}
+
+.location__country-name {
+  margin-bottom: rem-calc(24);
+  font-size: rem-calc(28);
+  line-height: rem-calc(36);
+}
+</style>
---
+++ src/components/LocationList.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="location-list">
+    <Location
+      :key="location.image"
+      v-for="location in locations"
+      :image-src="location.image"
+      :country="location.country"
+      :city="location.city"
+      :street-number="location.streetNumber"
+      :postal-code="location.postalCode"
+      :contact-number="location.contactNumber"
+    />
+  </div>
+</template>
+
+<script>
+import Location from "@/components/Location.vue";
+export default {
+  name: "LocationList",
+  components: {
+    Location,
+  },
+  data() {
+    return {
+      locations: [
+        {
+          image: "about/desktop/illustration-uk.svg",
+          country: "United kingdom",
+          city: "Alcaston",
+          streetNumber: "68 Asfordby Rd",
+          postalCode: "SY6 1YA",
+          contactNumber: "+44 1241 918425",
+        },
+        {
+          image: "about/desktop/illustration-canada.svg",
+          country: "Canada",
+          city: "Toronto",
+          streetNumber: "1528  Eglinton Avenue",
+          postalCode: "M4P 1A6",
+          contactNumber: "+1 416 485 2997",
+        },
+        {
+          image: "about/desktop/illustration-australia.svg",
+          country: "Australia",
+          city: "Kewell",
+          streetNumber: "36 Swanston Street",
+          postalCode: "SY6 1YA",
+          contactNumber: "+61 4 9928 3629",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.location-list {
+  @media screen and (min-width: $breakpoint-tablet) {
+    display: flex;
+    &:not(:last-child) {
+      margin-bottom: rem-calc(0);
+      margin-right: rem-calc(95);
+    }
+  }
+}
+</style>
