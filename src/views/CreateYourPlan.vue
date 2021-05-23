e2c64910ec60667a4945deff51199e004ecb40a7
Jovan Medford <jovanmedford@gmail.com>
2021-05-23 11:26:11 -0400

    Feat: Add route for subscription page

---
+++ src/views/CreateYourPlan.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="create-your-plan">
+    <Header />
+    <BaseHero
+      copy="Great coffee made simple."
+      text="Start your mornings with the world’s best coffees. 
+            Try our expertly curated artisan coffees from our best roasters 
+            delivered directly to your door, at your schedule."
+      button-text="Create your plan"
+      img="home/mobile/image-hero-coffeepress.jpg"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreateYourPlan",
+};
+</script>
+
+<style lang="scss" scoped></style>
